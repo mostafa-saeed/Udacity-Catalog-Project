a6511015feac896ad48325173bb11c2864f24584
Mostafa Saeed <mostafa.saeed543@gmail.com>
2017-11-29 17:00:24 +0200

    fixes

--- database_setup.py
+++ database_setup.py
@@ -1,13 +1,14 @@
 import sys, os
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'class'))
-from main import Base
+
+Base = declarative_base()
 
 DB_SERVER = 'localhost'
 DB_USER = 'catalog'
 DB_NAME = 'catalog'
-DB_PASSWORD = ''
+DB_PASSWORD = '123'
 CONNECTION_STRING = 'postgresql+psycopg2://' + DB_USER + ':' + DB_PASSWORD + '@' + DB_SERVER + '/' + DB_NAME
 
 engine = create_engine(CONNECTION_STRING)
