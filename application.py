761ee2ef2d500aa654916bef2cc0e9cd74a355a9
Mostafa Saeed <mostafa.saeed543@gmail.com>
2017-11-29 18:51:59 +0200

    update

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
+from flask import Flask, render_template, request, redirect, jsonify, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, CONNECTION_STRING
 
 from classes.user import User
 from classes.item import Item
 
-from database_setup import Base, CONNECTION_STRING
 
 engine = create_engine(CONNECTION_STRING)
 Base.metadata.bind = engine
@@ -13,7 +15,7 @@ session = DBSession()
 
 
 
-from flask import Flask, render_template, request, redirect, jsonify, url_for
+#=======================================================================
 
 
 
@@ -23,11 +25,5 @@ def getAllItems():
     items = session.query(Item).all()
     return jsonify(items=[c.serialize for c in items])
 
-
-
-
-
 app.debug = True
 app.run(host='0.0.0.0', port=5000)
-
-#=======================================================================
